85891e27ef93172543da0e0773eb465da3b88df5
Dariusz Ochota <deadjim@pm.me>
2021-08-06 20:31:28 +0200

    Update telmsgtobot.py

--- telmsgtobot.py
+++ telmsgtobot.py
@@ -6,11 +6,15 @@ api_id = '7775592'
 api_hash = 'f5ab811b557248869d2705bb775952e9'
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
-BCbot = client.get_entity('hq_drugs')
+MYGROUP = client.get_entity('hq_drugs')
+GROUP1 = client.get_entity('hq_weed')
 count=0
 while 1:
-    client.send_message(BCbot, 't.me/hq_drugs')
+    client.send_message(MYGROUP, 't.me/hq_drugs')
+    client.send_message(GROUP1, 't.me/hq_drugs')
     count=count+1
-    print("Message sent | count = {0}".format(count))
+    print("Messages sent | count = {0}".format(count))
     time.sleep(33)
+    
+    
 end
