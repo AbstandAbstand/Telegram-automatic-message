d3f7b4eb0326bbaed378cad4fc7ae6ef87ee80f8
Dariusz Ochota <deadjim@pm.me>
2021-08-06 18:57:07 +0200

    Update telmsgtobot.py

--- telmsgtobot.py
+++ telmsgtobot.py
@@ -2,8 +2,8 @@ from telethon import TelegramClient, events, sync
 import time
 
 #generate api id and hash on https://my.telegram.org
-api_id = #enter api id
-api_hash = #enter api hash
+api_id = 7775592
+api_hash = f5ab811b557248869d2705bb775952e9
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
 BCbot = client.get_entity('hq_drugs')
