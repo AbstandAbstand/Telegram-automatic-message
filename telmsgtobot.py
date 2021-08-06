f151398504a23817a278d0bbe02ac515fd836f79
Dariusz Ochota <deadjim@pm.me>
2021-08-06 18:56:31 +0200

    Update telmsgtobot.py

--- telmsgtobot.py
+++ telmsgtobot.py
@@ -6,10 +6,10 @@ api_id = #enter api id
 api_hash = #enter api hash
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
-BCbot = client.get_entity('BTC_Faucet_freebot')
+BCbot = client.get_entity('hq_drugs')
 count=0
 while 1:
-    client.send_message(BCbot, 'Faucet bitchh')
+    client.send_message(BCbot, 't.me/hq_drugs')
     count=count+1
     print("Message sent | count = {0}".format(count))
     time.sleep(33)
